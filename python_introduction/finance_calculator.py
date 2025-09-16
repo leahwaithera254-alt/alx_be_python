f1dc18806b0240cb1d552f12baf358f5d00d989f
leahwaithera254-alt <leahwaithera254@gmail.com>
2025-09-16 22:18:38 +0300

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -7,9 +7,10 @@ monthly_savings = float(income) - float(expenses)
 print("Your monthly savings are", monthly_savings)
 
 # Project savings over a year with 5% interest
-projected_savings = monthly_savings * 12 * 1.05  # Adds 5% interest
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 # Display results
 print("Projected savings after one year, with interest, is:", projected_savings)
+
 
 
